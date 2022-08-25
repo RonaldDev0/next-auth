ed5626ef998469108452a24d01dc53ac621ebf28
Ronald <ronaldzamoradev@gmail.com>
2022-08-24 20:05:43 -0500

    .

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,12 +5,12 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: 'd35f19b1550dae0b79e7',
+      clientSecret: '0bfbd327c9b434cda5d0c18bdd291b8092b81823',
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientId: '406401004053-fssikkriu9e2egio5oaregavs2je7o8h.apps.googleusercontent.com',
+      clientSecret: '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'
     })
     
   ],
